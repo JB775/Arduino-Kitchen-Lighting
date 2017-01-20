0dcd27a48243584595d7fa524d9e92a7bc96f38b
John Bergandino <jbergandino@gmail.com>
2017-01-19 23:59:10 -0500

    Add some basic logic to initially test hardware

--- arduino-kitchen-lighting.ino
+++ arduino-kitchen-lighting.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
+const int buttonPin = 2;
+const int ledPin =  13;
+int buttonState = 0;
 
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  buttonState = digitalRead(buttonPin);
 
+  if (buttonState == HIGH) {
+    // turn lights on:
+    digitalWrite(ledPin, HIGH);
+    Serial.print("Lights on.");
+
+  } else {
+    // turn lights off:
+    digitalWrite(ledPin, LOW);
+    Serial.print("Lights off.");
+  }
 }
