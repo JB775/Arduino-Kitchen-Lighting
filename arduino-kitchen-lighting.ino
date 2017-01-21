3b9fed0ac26ad5975cb623c6845dca4f4fd4ca68
John Bergandino <jbergandino@gmail.com>
2017-01-21 00:29:47 -0500

    Add state change detection and 100ms button debounce

--- arduino-kitchen-lighting.ino
+++ arduino-kitchen-lighting.ino
@@ -1,24 +1,43 @@
-const int buttonPin = 2;
-const int ledPin =  13;
-int buttonState = 0;
+
+const int  buttonPin = 7; 
+const int ledPin = 13;    
+int buttonPushCounter = 1;
+int buttonState = 0;      
+int lastButtonState = 0;  
+
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
 }
+
 
 void loop() {
-  
   buttonState = digitalRead(buttonPin);
 
-  if (buttonState == HIGH) {
-    // turn lights on:
-    digitalWrite(ledPin, HIGH);
-    Serial.print("Lights on.");
+  if (buttonState != lastButtonState) {
+    
+    if (buttonState == HIGH) {
+   
+      buttonPushCounter++;
+      Serial.println("Lights On");
+      Serial.print("number of button pushes:  ");
+      Serial.println(buttonPushCounter);
+    } else {
+   
+      Serial.println("Lights Off");
+    }
+ 
+    delay(100);  // debounce
+  }
+  lastButtonState = buttonState;
 
+  // Turn the lights on/off with every button push
+  if (buttonPushCounter % 2 == 0) {
+    digitalWrite(ledPin, HIGH);
   } else {
-    // turn lights off:
     digitalWrite(ledPin, LOW);
-    Serial.print("Lights off.");
   }
 }
+
