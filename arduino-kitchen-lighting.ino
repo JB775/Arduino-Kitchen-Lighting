21422229832a2feac6f926fc97acb3379e636d59
John Bergandino <jbergandino@gmail.com>
2017-01-22 20:53:13 -0500

    Add additional pins, monitor current button state, add additional debounce delay

--- arduino-kitchen-lighting.ino
+++ arduino-kitchen-lighting.ino
@@ -1,43 +1,73 @@
 
 const int  buttonPin = 7; 
-const int ledPin = 13;    
+const int ledPin1 = 13;
+const int ledPin2 = 8; 
+const int ledPin3 = 10; 
+const int ledPin4 = 12; 
+const int ledPin5 = 2; 
 int buttonPushCounter = 1;
-int buttonState = 0;      
-int lastButtonState = 0;  
-
+int buttonActivated = 0;      
+int lastButtonActivated = 0;  
+int currentbuttonState = 0;
 
 void setup() {
   pinMode(buttonPin, INPUT);
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
+  pinMode(ledPin5, OUTPUT);
   Serial.begin(9600);
 }
 
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
+  buttonActivated = digitalRead(buttonPin);
 
-  if (buttonState != lastButtonState) {
+  if (buttonActivated != lastButtonActivated) {
     
-    if (buttonState == HIGH) {
+    if (buttonActivated == HIGH) {
    
       buttonPushCounter++;
-      Serial.println("Lights On");
+//      Serial.println("Lights On");
       Serial.print("number of button pushes:  ");
-      Serial.println(buttonPushCounter);
-    } else {
-   
-      Serial.println("Lights Off");
-    }
- 
-    delay(100);  // debounce
+      Serial.println(buttonPushCounter-1);
+      if (currentbuttonState) {
+        Serial.println("Lights Triggered Off");
+      } else {
+        Serial.println("Lights Triggered On");
+      }
+    } 
+    
+    delay(150);  // debounce
   }
-  lastButtonState = buttonState;
+
+  lastButtonActivated = buttonActivated;
 
   // Turn the lights on/off with every button push
   if (buttonPushCounter % 2 == 0) {
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin1, HIGH);
+    digitalWrite(ledPin2, HIGH);
+    digitalWrite(ledPin3, HIGH);
+    digitalWrite(ledPin4, HIGH);
+    digitalWrite(ledPin5, HIGH);
+    currentbuttonState = 1;
+    
   } else {
-    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin2, LOW);
+    digitalWrite(ledPin3, LOW);
+    digitalWrite(ledPin4, LOW);
+    digitalWrite(ledPin5, LOW);
+    currentbuttonState = 0;
+  }
+
+  // Reset push counter so it remains a low number
+  if (buttonPushCounter == 5) {
+    buttonPushCounter = 1;
+    delay(50);  // debounce
   }
 }
+
+
 
