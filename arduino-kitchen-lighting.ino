a4892765711210886c614a6692989449cfe9d247
John Bergandino <jbergandino@gmail.com>
2017-02-06 23:46:47 -0500

    Update pins in code to match actual hardware schematic. Remove serial code since I do not need that on ATtiny85.  Add Arduino as ISP hookup diagram and ATTiny85 pinout.

--- arduino-kitchen-lighting.ino
+++ arduino-kitchen-lighting.ino
@@ -1,14 +1,11 @@
-
-const int  buttonPin = 7; 
-const int ledPin1 = 13;
-const int ledPin2 = 8; 
-const int ledPin3 = 10; 
-const int ledPin4 = 12; 
-const int ledPin5 = 2; 
+const int buttonPin = 3; 
+const int ledPin1 = 0;
+const int ledPin2 = 1; 
+const int ledPin3 = 2; 
+const int ledPin4 = 4; 
 int buttonPushCounter = 1;
 int buttonActivated = 0;      
 int lastButtonActivated = 0;  
-int currentbuttonState = 0;
 
 void setup() {
   pinMode(buttonPin, INPUT);
@@ -16,58 +13,33 @@ void setup() {
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin3, OUTPUT);
   pinMode(ledPin4, OUTPUT);
-  pinMode(ledPin5, OUTPUT);
-  Serial.begin(9600);
 }
-
 
 void loop() {
   buttonActivated = digitalRead(buttonPin);
-
   if (buttonActivated != lastButtonActivated) {
-    
     if (buttonActivated == HIGH) {
-   
       buttonPushCounter++;
-//      Serial.println("Lights On");
-      Serial.print("number of button pushes:  ");
-      Serial.println(buttonPushCounter-1);
-      if (currentbuttonState) {
-        Serial.println("Lights Triggered Off");
-      } else {
-        Serial.println("Lights Triggered On");
-      }
     } 
-    
-    delay(150);  // debounce
+    delay(150);
   }
 
   lastButtonActivated = buttonActivated;
 
-  // Turn the lights on/off with every button push
   if (buttonPushCounter % 2 == 0) {
     digitalWrite(ledPin1, HIGH);
     digitalWrite(ledPin2, HIGH);
     digitalWrite(ledPin3, HIGH);
     digitalWrite(ledPin4, HIGH);
-    digitalWrite(ledPin5, HIGH);
-    currentbuttonState = 1;
-    
   } else {
     digitalWrite(ledPin1, LOW);
     digitalWrite(ledPin2, LOW);
     digitalWrite(ledPin3, LOW);
     digitalWrite(ledPin4, LOW);
-    digitalWrite(ledPin5, LOW);
-    currentbuttonState = 0;
   }
 
-  // Reset push counter so it remains a low number
   if (buttonPushCounter == 5) {
     buttonPushCounter = 1;
-    delay(50);  // debounce
+    delay(50);
   }
 }
-
-
-
